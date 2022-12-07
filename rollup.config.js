19eb7e75d76273288bc2bcf75d21a3ce70546b91
Jeffpreussner <jgpreussner@gmail.com>
2022-12-07 04:29:38 -0500

    linting

--- rollup.config.js
+++ rollup.config.js
@@ -77,21 +77,21 @@ const rollupPlugins = R.reject(
     babel({
       exclude: "node_modules/**",
       babelHelpers: "bundled",
-      // presets: [
-      //   [
-      //     "@babel/preset-env",
-      //     {
-      //       useBuiltIns: "usage",
-      //       corejs: 3,
-      //     },
-      //   ],
-      //   [
-      //     "@babel/preset-react",
-      //     {
-      //       runtime: "automatic",
-      //     },
-      //   ],
-      // ],
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            useBuiltIns: "usage",
+            corejs: 3,
+          },
+        ],
+        [
+          "@babel/preset-react",
+          {
+            runtime: "automatic",
+          },
+        ],
+      ],
     }),
     dynamicImportVar({ warnOnError: true }),
     commonjs({
